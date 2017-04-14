e88cf35146281922b66051e49c0e89162043edfe
TessieTeng <tessie.teng@sunlight-tech.com>
2017-04-14 14:36:45 +0800

    项目提交

--- epg/vuex/actions.js
+++
@@ -1,8 +0,0 @@
-export const updateHomeTab = function ({ dispatch},newHometab) {
-    dispatch("updateHomeTab", newHometab);
-}
-
-export const updateClassTab = function({dispatch},newClassTab){
-  dispatch("updateClassTab", newClassTab);
-}
-
