0c63b34504e0ad22d18209315915d4b647953d4f
hedgehog <465016678@qq.com>
2017-05-02 15:42:36 +0800

    修复登录不了的bug

--- EPG_Login_Auth/components/Vues/App.vue
+++ EPG_Login_Auth/components/Vues/App.vue
@@ -89,8 +89,7 @@ export default {
                 }
                 switch (sessionStorage.getItem('province')) {
                     case '云南':
-                        tmpBody.USERID = Authentication.CTCGetConfig("UserID");
-                        tmpBody.USERID = "";
+                        tmpBody.USERID = window.Authentication ? Authentication.CTCGetConfig("UserID") : '';
                         break;
                     case '湖北':
                         tmpBody.USERID = this.oss_user_id;
