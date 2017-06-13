c221b774255e315492728f0e00a6b3744a901f46
lizc <gccll.love@gmail.com>
2017-06-13 19:18:57 +0800

    兼容黄框问题

--- EPG_Welcome/components/App.vue
+++ EPG_Welcome/components/App.vue
@@ -25,6 +25,13 @@ export default {
         },
         store: store,
         ready() {
+
+            // 茁壮中间件默认焦点框问题
+            console.log('ipanel: ' + iPanel);
+            if (!!window.iPanel) {
+                iPanel.focusWidth = 0;
+            }
+            
             // var html = document.getElementsByTagName('html')[0];
             // var width = html.offsetWidth;
             // html.style.fontSize = (width >= 1920 ? 1920 : width) / 1920 * 100 + 'px';
