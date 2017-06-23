24b45d3b38633f5651b27e42054dced22957cdd4
lizc <gccll.love@gmail.com>
2017-06-23 11:38:51 +0800

    修改版本信息显示布局和内容

--- EPG_Android/components/Vues/VersionInfo.vue
+++ EPG_Android/components/Vues/VersionInfo.vue
@@ -1,44 +1,80 @@
 <style scoped>
 
-.versionInfo {
-    position: fixed;
-    left: 0px;
-    top: 0px;
+* {
+    margin: 0;
+    padding: 0;
+}
+
+#versionInfo {
+    background: rgb(60, 60, 60);
+    position: fixed;;
+    left: 0;
+    top: 0;
     width: 1280px;
     height: 720px;
-    background: black;
-    color: white;
 }
+
+.container {
+    text-align: left;
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    padding: 20px;
+    color: #fff;
+    overflow: hidden;
+}
+
+.container p {
+    margin: 10px 0 10px;
+}
+
+.list {
+    margin: 10px 0 10px;
+}
+
+.list ul li {
+    list-style-type: none;
+    margin: 10px;
+
+}
+
 </style>
 <!--******************EPG版本信息页*******************-->
 <template>
     <div id="versionInfo" class="versionInfo">
-        <h1>版本信息</h1>
-        <span>{{versionNo}}</span>
-        <span>{{versionDesc}}</span>
-        <ol id="debuggers">
-            <li v-for="bugItem in versionDuggers">{{bugItem}}</li>
-        </ol>
+        <div class="container">
+            <h2>版本号：{{version.no}}</h2>
+            <p v-if="version.desc">{{version.desc}}</p>
+            <div class="list" v-if="version.buglist && version.buglist.length>0">
+                <h2>修复的问题列表：</h2>
+                <ul>
+                    <li v-for="(index, bug) in version.buglist">{{index+1}}、 {{bug}}</li>
+                </ul>
+            </div>
+            <div class="list" v-if="version.features && version.features.length>0">
+                <h2>新增功能：</h2>
+                <ul>
+                    <li v-for="(index, fea) in version.features">{{index+1}}、 {{fea}}</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-
-            // 组合键处理器
-            versionNo: 'v1.0.0',
-            versionDesc: '版本描述',
-            versionDuggers: [
-                '解决问题一',
-                '解决问题二',
-                '解决问题三'
-            ],
+            version: {
+                no: 'v1.0.1',
+                desc: '此次版本升级是为了获得更好的用户体验。',
+                buglist: ['解决视频不能播放问题', '解决焦点黄框问题', '解决视频播放卡顿问题'],
+                features: ['开机广告视频', '主页面背景视频及其循环播放', '频道直播页面'],
+            },    
         };
     },
 
     ready() {
-        console.log('version info vue ready ...');
+        this.version = JSON.parse(sessionStorage.getItem('SYEpgVersion'));
     },
 }
 </script>
